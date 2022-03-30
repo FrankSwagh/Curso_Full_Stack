9cbb450dcaa7c2f9a6c675bc0984b46b9fc34fbe
Francisco Cabrera <63220126+FrankSwagh@users.noreply.github.com>
2022-03-29 20:18:07 -0600

    Add files via upload

--- Parte 2/Phonebook/App.js
+++ Parte 2/Phonebook/App.js
@@ -1,105 +1,67 @@
-import React, { useState } from "react";
-import ImprimirBusqueda from "./components/ImprimirBusqueda";
-import Busqueda from "./components/Buscar";
-import AgregarNombre from "./components/AgregarNombre";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
-  const [newName, setNewName] = useState("");
-  const [newPhone, setPhone] = useState("");
-  const [newSearch, setSearch] = useState("");
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const ImprimirNombres = (event) => {
-    event.preventDefault();
-    let sinos = Alerta();
-    if (sinos) {
-      const nuevoObjeto = {
-        name: newName,
-        number: newPhone,
-        id: persons.length + 1,
-      };
-      console.log("verdadero");
-      setPersons(persons.concat(nuevoObjeto));
-      setNewName("");
-      setPhone("")
-    } else {
-      console.log("falso");
-      setNewName("");
-      setPhone("");
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
-    return "";
-  };
 
-  const Alerta = () => {
-    let sino = false;
-    persons.map((indice) => {
-      let nombres = [];
-      nombres.push(indice.name);
-      if (nombres.includes(newName)) {
-        window.alert(`${newName} is already in phonebook`);
-        sino = false;
-      } else {
-        sino = true;
-      }
-      return sino;
-    });
-    return sino;
-  };
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
-  const buscarPersona = (event) => {
-    setSearch(event.target.value);
-  };
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
-  const cambiarNombre = (event) => {
-    setNewName(event.target.value);
-  };
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
-  const cambiarTelefono = (event) => {
-    setPhone(event.target.value);
-  };
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Busqueda buscarPersona={buscarPersona} />
-      <h2>Add a new</h2>
-      <AgregarNombre
-        ImprimirNombres={ImprimirNombres}
-        newName={newName}
-        newPhone={newPhone}
-        cambiarNombre={cambiarNombre}
-        cambiarTelefono={cambiarTelefono}
-      />
-
-      <h2>Numbers</h2>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
       <ul>
-        <ImprimirBusqueda persons={persons} newSearch={newSearch} />
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
-    </div>
-  );
-};
-
-export default App;
-/*
-<form onSubmit={buscarPersona}>
-        <div>
-          filter show with{" "}
-          <input value={newSearch} onChange={cambiarBusqueda} />
-        </div>
-      </form>
-
-      
-        <ImprimirNombres
-          Alerta={Alerta}
-          persons={persons}
-          newName={newName}
-          newPhone={newPhone}
-          cambiarNombre={cambiarNombre}
-          cambiarTelefono={cambiarTelefono}
-          CambiarArreglo={CambiarArreglo}
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
         />
-*/
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
